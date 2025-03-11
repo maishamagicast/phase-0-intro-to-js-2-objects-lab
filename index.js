9f8f6da1d9b5aa3ee0eb5f6c77cb094dbd803643
ElvisNdunguMbuthia <maishamagicast@gmail.com>
2025-03-11 20:07:27 +0300

    Basic Calcultaor

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Sam',
+    streetAddress: '11 Broadway' ,
+} ;
+
+
+function updateEmployeeWithKeyAndValue (employee, key, value) {
+    const newEmployee = { ...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue (employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey (employee,key,value) {
+    const newEmployee2 = { ...employee};
+    delete newEmployee2[key];
+    return newEmployee2;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee[key];
+    return employee;
+}
